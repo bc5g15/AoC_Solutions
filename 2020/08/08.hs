c8ab1d6bbeced3da671024ad5781d62331958aea
Benedict Cairns <bcairns21@gmail.com>
2020-12-08 11:05:04 +0000

    2020 Day 8 part 2

--- 2020/08/08.hs
+++ 2020/08/08.hs
@@ -1,4 +1,5 @@
 import qualified Data.Set as Set
+import Data.Maybe (isJust)
 
 -- State = (PC, ACC)
 type State = (Int, Int)
@@ -9,6 +10,11 @@ main = do
     let code = (map ((\x -> (head x, readJump (head $ tail x))) . words) . lines) instring
     -- Part 1
     print $ runUntilRepeat code
+    -- Part 2
+    let k = zip code [0..]
+    let swapPos = map snd $ filter (\((x, _), _) -> x == "nop" || x == "jmp") k
+    let codesSwaps = map (swapOne code) swapPos
+    print . filter isJust $ map strictRun codesSwaps
 
 
 readJump :: String -> Int
@@ -23,7 +29,7 @@ process :: State -> Instruction -> State
 process (pc, acc) ("nop", _)    = (pc+1, acc)
 process (pc, acc) ("acc", val)  = (pc+1, acc+val)
 process (pc, acc) ("jmp", val)  = (pc+val, acc)
-proccess _ (x, _) = error "Unrecognized Instruction: " ++ x
+process _ (x, _) = error ("Unrecognized Instruction: " ++ x)
 
 runUntilRepeat :: [Instruction] -> State
 runUntilRepeat x = runInner x (0, 0) Set.empty
@@ -32,3 +38,21 @@ runUntilRepeat x = runInner x (0, 0) Set.empty
         runInner xs state@(pc, _) visited 
             | Set.member pc visited = state
             | otherwise = runInner xs (process state (xs !! pc)) (Set.insert pc visited)
+
+strictRun :: [Instruction] -> Maybe State
+strictRun xs = runInner xs (0, 0) Set.empty
+    where 
+        runInner :: [Instruction] -> State -> Set.Set Int -> Maybe State
+        runInner xs state@(pc, _) visited
+            | Set.member pc visited = Nothing
+            | pc == length xs = Just state
+            | pc < 0 || pc > length xs = Nothing
+            | otherwise = runInner xs (process state (xs !! pc)) (Set.insert pc visited)
+
+swap :: Instruction -> Instruction
+swap ("nop", x) = ("jmp", x)
+swap ("jmp", x) = ("nop", x)
+swap other = other
+
+swapOne :: [Instruction] -> Int -> [Instruction]
+swapOne xs i = take i xs ++ [swap (xs !! i)] ++ drop (i+1) xs
