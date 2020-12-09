19daf4111bd14461f085d7eb91ce7f0e69bbf2ed
Benedict Cairns <bcairns21@gmail.com>
2020-12-09 11:10:52 +0000

    2020 Day 9

---
+++ 2020/09/09.hs
@@ -0,0 +1,19 @@
+import Data.List (inits, tails)
+
+main = do
+    instring <- readFile "test.txt"
+    let inlines = map (\x -> read x::Int) $ lines instring
+    -- Part 1
+    let poss = possibilities 5 inlines
+    let part1 = fst . head. filter (null . snd) $ map (uncurry sums) poss
+    print part1
+    -- Part 2
+    let k = concatMap inits $ tails inlines
+    let result = head $ filter (\x -> sum x == part1 && length x >= 2) k
+    print (minimum result + maximum result)
+
+sums :: Int -> [Int] -> (Int, [(Int, Int)])
+sums r xs = (r, [(a, b) | a <- xs, b <- xs, a/=b, a+b==r])
+
+possibilities :: Int -> [Int] -> [(Int, [Int])]
+possibilities pre xs = [(xs !! n, take pre $ drop (n-pre) xs) | n <- [pre..length xs-1]]
