559203bca25c7da885015992aa6c12bca01f9510
Benedict Cairns <bcairns21@gmail.com>
2020-12-15 19:23:37 +0000

    Day 15 part 2

--- 2020/15/15.hs
+++ 2020/15/15.hs
@@ -1,12 +1,25 @@
 import qualified Data.Map.Strict as Map
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
+
 main = do 
     let innums = [6,4,12,1,20,0,16]
 
     let start = initialRead innums
+    -- part 1
     print . (\(x,y,_) -> (x,y)) $ iterate takeTurn start !! (2020 - length innums)
+    -- part 2
+    startTime <- getCurrentTime
+    print . (\(_,y,_) -> y) $ runUntilTurn start 30000000
+    endTime <- getCurrentTime
+    print $ diffUTCTime endTime startTime -- ~90s
 
 -- State = (Turn, LastSaid, Memory)
 type State = (Int, Int, Map.Map Int Int)
+
+runUntilTurn :: State -> Int -> State
+runUntilTurn state@(t, _, _) x
+    | t == x = state
+    | otherwise = runUntilTurn (takeTurn state) x
 
 initialRead :: [Int] -> State 
 initialRead xs = (t, l, m)
