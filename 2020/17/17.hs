22c8eea5f2f8270b3010e3fc9a7ced226d9ed584
Benedict Cairns <bcairns21@gmail.com>
2020-12-17 12:33:01 +0000

    Day 17 Part 2

--- 2020/17/17.hs
+++ 2020/17/17.hs
@@ -4,23 +4,15 @@ main :: IO ()
 main = do
     instring <- readFile "in.txt"
     let inlines = lines instring
+    -- Part 1
     let state = Set.fromList $ readInitialState inlines 0 0 0
-
-    -- print $ neighbours (0, 0, 0)
-    -- print $ Set.size state
-    -- print state
-    -- print $ countNeighbours (0, 1, -1) state
     print . Set.size $ iterate turn state !! 6
-    -- print . Set.filter (\(_,_,z) -> z==0) $ turn state
-    -- print $ countNeighbours (1, 0, 0) state
-    -- print . filter (`Set.member` state) $ neighbours (1,0,0)
-
-    -- print $ neighboursA [0, 0]
+    -- Part 2
+    let state2 = Set.fromList $ aReadInitialState inlines [0,0,0,0]
+    print . Set.size $ iterate aTurn state2 !! 6
 
 type Cube = (Int, Int, Int)
-type Hypercube = (Int, Int, Int, Int)
 type Cubes = Set.Set Cube
-type Hypercubes = Set.Set Hypercube
 
 readInitialState :: [String] -> Int -> Int -> Int -> [Cube]
 readInitialState [] _ _ _ = []
@@ -30,7 +22,6 @@ readInitialState ((a:as):bs) x y z
     | otherwise = readInitialState (as:bs) (x+1) y z
 
 neighbours :: Cube -> [Cube]
-
 neighbours (x, y, z) = [(i, j, k) | i<-[x-1..x+1], j<-[y-1..y+1], k<-[z-1..z+1], not (i==x && j==y && k==z)]
 
 countNeighbours :: Cube -> Cubes -> Int
@@ -56,15 +47,50 @@ turn cs = result
         result = Set.fromList $ filter (check cs) fullCoords
 
 -- Dimension agnostic version
--- type Coord = [Int]
--- type Active = Set.Set Coord
+type Coord = [Int]
+type Active = Set.Set Coord
 
--- agNeighbours :: Coord -> [Coord]
--- agNeighbours x = filter (/=x) $ neighboursA x
+aReadInitialState :: [String] -> [Int] -> [Coord]
+aReadInitialState [] _ = []
+aReadInitialState ([]:as) (x:y:os) = aReadInitialState as (0:(y+1):os)
+aReadInitialState ((a:as):bs) (x:y:os)
+    | a=='#' = (x:y:os) : next
+    | otherwise = next
+    where
+        next = aReadInitialState (as:bs) ((x+1):y:os)
 
--- neighboursA :: [Int] ->[[Int]]
--- neighboursA [a] = [[a-1], [a], [a+1]]
--- neighboursA (a:as) = map (a-1:) (neighboursA as) ++ 
---     map (a:) (neighboursA as) ++ map (a+1:) (neighboursA as)
+agNeighbours :: Coord -> [Coord]
+agNeighbours x = filter (/=x) $ neighboursA x
 
--- countNei
+neighboursA :: [Int] ->[[Int]]
+neighboursA [a] = [[a-1], [a], [a+1]]
+neighboursA (a:as) = map (a-1:) (neighboursA as) ++ 
+    map (a:) (neighboursA as) ++ map (a+1:) (neighboursA as)
+
+aCountNeighbours :: Coord -> Active -> Int
+aCountNeighbours c cs = length . filter (==True) . map (`Set.member` cs) $ agNeighbours c
+
+aCheckRange :: Active -> (Coord, Coord)
+aCheckRange cs = (mins, maxes)
+    where 
+        myMin = minBound::Int
+        myMax = maxBound::Int
+        mins = Set.foldl' (\x y -> eat x y min) (repeat myMax) cs
+        maxes = Set.foldl' (\x y -> eat x y max) (repeat myMin) cs
+        eat :: Coord -> Coord -> (Int -> Int -> Int) -> Coord
+        eat [] _ _ = []
+        eat _ [] _ = []
+        eat (a:as) (b:bs) f = f a b : eat as bs f
+
+aTurn :: Active -> Active
+aTurn cs = result
+    where
+        (mins, maxes) = aCheckRange cs
+        fullCoords :: Coord -> Coord -> [Coord]
+        fullCoords [] [] = [[]]
+        fullCoords (a:smalls) (b:bigs) = [(x:)|x<-[a-1..b+1]] <*> fullCoords smalls bigs
+        check :: Active -> Coord -> Bool
+        check cs c
+            | Set.member c cs = aCountNeighbours c cs `elem` [2,3]
+            | otherwise = aCountNeighbours c cs == 3
+        result = Set.fromList $ filter (check cs) (fullCoords mins maxes)
