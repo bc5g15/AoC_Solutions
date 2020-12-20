d9d03726f172f89492eb8470d79951152683ca4b
Benedict Cairns <bcairns21@gmail.com>
2020-12-20 17:32:29 +0000

    Day 20 Part 2

--- 2020/20/20-full.hs
+++ 2020/20/20-full.hs
@@ -1,7 +1,7 @@
 import Data.List (transpose)
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set 
 import Data.List.Split (splitOn)
-import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -10,12 +10,31 @@ main = do
 
     let tiles = map (\x -> (takeTitle (head x), tail x)) $ splitOn [""] inlines
 
+    -- Part 1
     let puzzle = buildPuzzle (head tiles) (tail tiles)
     print . product $ corners puzzle
 
+    -- Part 2
+    let puzString = joinPuzzle puzzle
+
+    let flips = [id, reverse, map reverse, reverse . map reverse]
+    let turns = [id, reverse.transpose, reverse.transpose.reverse.transpose, reverse.transpose.reverse.transpose.reverse.transpose]
+
+    let transforms = [x . y | x<-flips, y<-turns]
+    let monsters = map (\f -> f monStr) transforms
+    let maxx = length $ head puzString
+    let maxy = length puzString
+    let puzSet = strToSet puzString
+    let monSets = map strToSet monsters
+    let monCount = maximum (map (\x -> monSearch x puzSet (maxx, maxy)) monSets)
+    
+    -- Final calculations!
+    let monSize = Set.size $ head monSets
+    let puzSize = Set.size puzSet 
+    print (puzSize - (monCount * monSize))
+
 type Piece = (Int, [String])
 type Puzzle = Map.Map (Int, Int) Piece
-type Shift = Piece -> Piece
 
 corners :: Puzzle -> [Int]
 corners p = [fst $ p Map.! (x, y)| x<-[minx,maxx], y<-[miny,maxy]]
@@ -43,9 +62,9 @@ clockwise (x, v) = (x, reverse $ transpose v)
 
 checkMatch :: Piece -> Piece -> (Int, Int) 
 checkMatch (_, v1) (_, v2)
-    | t v1 == b v2 = (0, 1)
+    | t v1 == b v2 = (0,-1)
     | r v1 == l v2 = (1, 0)
-    | b v1 == t v2 = (0,-1)
+    | b v1 == t v2 = (0,1)
     | l v1 == r v2 = (-1,0)
     | otherwise = (0, 0)
     where
@@ -70,10 +89,48 @@ buildPuzzle p ps = inner (Map.singleton (0, 0) p) p (0, 0) ps [] []
     where
         inner :: Puzzle -> Piece -> (Int, Int) -> [Piece] -> [Piece] -> [(Int, Int)] -> Puzzle
         inner acc _ _ [] [] [] = acc
-        -- inner acc _ (x, y) [] seen [] = acc
         inner acc _ _ [] seen (t:todo) =  inner acc (acc Map.! t) t seen [] todo
         inner acc o (x, y) (p:ps) seen todo = case am of 
             Nothing -> inner acc o (x, y) ps (p:seen) todo
             Just ((i, j), np) -> inner (Map.insert (x+i, y+j) np acc) o (x, y) ps seen ((x+i,y+j):todo)
             where
                 am = anyMatch o p
+
+-- Part 2 
+stripEdges :: Piece -> Piece
+stripEdges (x, n) = (x, tail . init $ map (tail . init) n)
+
+joinPuzzle :: Puzzle -> [String]
+joinPuzzle n = final
+    where
+        keys = Map.keys n
+        minx = minimum $ map fst keys
+        maxx = maximum $ map fst keys
+        miny = minimum $ map snd keys
+        maxy = maximum $ map snd keys
+        enrow = \y -> [snd . stripEdges $ n Map.! (x, y) | x<-[minx..maxx]]
+        rows = map enrow [miny..maxy]
+        rowJoin :: [[String]] -> [String]
+        rowJoin ([]:_) = []
+        rowJoin ms@(_:_) = concatMap head ms : rowJoin (map tail ms)
+        final = concatMap rowJoin rows
+
+strToSet :: [String] -> Set.Set (Int, Int) 
+strToSet xs = Set.fromList (innery 0 xs)
+    where
+        innerx :: Int -> Int -> String -> [(Int, Int)]
+        innerx _ _ [] = []
+        innerx i j ('#':xs) = (i, j) : innerx (i+1) j xs
+        innerx i j (_:xs) = innerx (i+1) j xs
+        innery :: Int -> [String] -> [(Int, Int)]
+        innery _ [] = []
+        innery j (x:xs) = innerx 0 j x ++ innery (j+1) xs
+
+monStr :: [String]
+monStr = [
+    "                  # ",
+    "#    ##    ##    ###",
+    " #  #  #  #  #  #   " ]
+
+monSearch :: Set.Set (Int, Int) -> Set.Set (Int, Int) -> (Int, Int) -> Int 
+monSearch mons stage (maxx, maxy)= length [(x,y)|x<-[0..maxx], y<-[0..maxy], Set.size (Set.intersection (Set.map (\(a, b) -> (a+x, b+y)) mons) stage) == Set.size mons ]
