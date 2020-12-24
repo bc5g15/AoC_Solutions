235302953ec5440c8f09101d7576296833e5fd48
Benedict Cairns <bcairns21@gmail.com>
2020-12-24 12:00:50 +0000

    Day 24 part 2

--- 2020/24/24.hs
+++ 2020/24/24.hs
@@ -3,21 +3,19 @@ import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-    instring <- readFile "test.txt"
+    instring <- readFile "in.txt"
     let inlines = lines instring
     let poss = map (resolvePos . eatDirList) inlines
-    print poss
+
+    -- Part 1
     let tiles = flipMany poss Set.empty
     print . Set.size $ tiles
 
     -- Part 2
-    print . Set.size $ fullCheck tiles
-    print "Hello"
+    print . Set.size $ iterate fullCheck tiles !! 100
 
--- 241 -- Too low
--- 307 -- Correct!
 
-type HexCoord = (Int, Int, Int)
+type HexCoord = (Int, Int)
 
 type CoordSet = Set.Set HexCoord
 
@@ -25,19 +23,19 @@ eatDirList :: String -> [HexCoord]
 eatDirList [] = []
 eatDirList (a:b:xs) = case a of
     'n' -> case b of
-        'w' -> (-1, 0, 1) : eatDirList xs
-        'e' -> (0, 1, 1) : eatDirList xs
+        'w' -> (-1, 1) : eatDirList xs
+        'e' -> (0, 1) : eatDirList xs
     's' -> case b of 
-        'w' -> (0, -1, -1) : eatDirList xs
-        'e' -> (1, 0, -1) : eatDirList xs
-    'w' -> (-1, -1, 0) : eatDirList (b:xs)
-    'e' -> (1,1,0) : eatDirList (b:xs)
+        'w' -> (0, -1) : eatDirList xs
+        'e' -> (1, -1) : eatDirList xs
+    'w' -> (-1, 0) : eatDirList (b:xs)
+    'e' -> (1,0) : eatDirList (b:xs)
 eatDirList [a] = case a of
-    'e' -> [(1, 1, 0)]
-    'w' -> [(-1, -1, 0)]
+    'e' -> [(1, 0)]
+    'w' -> [(-1, 0)]
 
 resolvePos :: [HexCoord] -> HexCoord
-resolvePos = foldl (\(x, y, z) (a, b, c) -> (x+a, y+b, z+c)) (0, 0, 0) 
+resolvePos = foldl (\(x, y) (a, b) -> (x+a, y+b)) (0, 0) 
 
 flipTile :: HexCoord -> CoordSet -> CoordSet
 flipTile c s 
@@ -48,26 +46,31 @@ flipMany :: [HexCoord] -> CoordSet -> CoordSet
 flipMany as s = foldl (flip flipTile) s as
 
 -- Part 2
+listNeighbours :: HexCoord -> [HexCoord]
+listNeighbours (a, b) = let nd = [(0,0), (-1, 1), (0,1), (1,0), (1,-1), (0,-1), (-1,0)] in
+    map (\(x,y) -> (x+a, y+b)) nd
 
 neighbours :: HexCoord -> CoordSet -> Int
-neighbours (a,b,c) s = length $ filter (`Set.member` s) nc
+neighbours (a,b) s = length $ filter (`Set.member` s) nc
     where
-        nd = [(-1, 0, 1), (0,1,1), (0,-1,-1), (1,0,-1), (-1,-1,0), (1,1,0)]
-        nc = map (\(x,y,z) -> (x+a, y+b, z+c)) nd
+        nd = [(-1, 1), (0,1), (1,0), (1,-1), (0,-1), (-1,0)]
+        nc = map (\(x,y) -> (x+a, y+b)) nd
+
+allToCheck :: CoordSet -> [HexCoord] 
+allToCheck cs = Set.elems . Set.unions $ map Set.fromList allN
+    where
+        lst = Set.elems cs
+        allN = map listNeighbours lst
 
 fullCheck :: CoordSet -> CoordSet
-fullCheck c = Set.fromList $ filter (`check` c) fullCoords
+fullCheck c = Set.fromList $ filter (`check` c) bigList
     where
-        maxes = (maxBound::Int, maxBound::Int, maxBound::Int)
-        mins = (minBound::Int, minBound::Int, minBound::Int)
-        (minA, minB, minC) = Set.foldl' (\(a,b,c) (x,y,z) -> (min a x, min b y, min c z)) maxes c
-        (maxA, maxB, maxC) = Set.foldl' (\(a,b,c) (x,y,z) -> (max a x, max b y, max c z)) mins c
-        fullCoords = [(a,b,c) | a<-[minA-1..maxA+1], b<-[minB-1..maxB+1], c<-[minC-1..maxC+1]]
+        bigList = allToCheck c
         check :: HexCoord -> CoordSet -> Bool
-        check h cs
+        check h cs 
             | Set.member h cs && (ns == 0 || ns > 2) = False
-            | Set.member h cs = trace (show ns) True 
-            | not $ Set.member h cs && ns == 2 = True
+            | Set.member h cs && ns>0 && ns <= 2 = True
+            | not (Set.member h cs) && ns == 2 = True 
             | otherwise = False 
             where
-                ns = neighbours h cs
+                ns = neighbours h cs 
