f8706bd87ed5576dff76f8069fd9a546d7663c7a
Benedict Cairns <bcairns21@gmail.com>
2021-12-07 05:39:10 +0000

    Day 7 - Part 2

--- 2021/07/7.js
+++ 2021/07/7.js
@@ -5,18 +5,23 @@ const visual = document.getElementById('visual')
 
 const positionCost = (x, positions) => positions.map(v => Math.abs(v-x)).reduce((acc, cur) => acc+cur, 0)
 
+const newFuelCost = (move) => move*(move+1)/2
+
+const newPositionCost = (x, positions) => positions.map(v => newFuelCost(Math.abs(v-x))).reduce((acc, cur) => acc+cur, 0)
+
 solveBtn.onclick = () => {
     const positions = puzzleInput.value.trim().split(',').map(v => parseInt(v, 10)).sort((a,b) => (a-b))
 
     const minPos = positions[0]
     const maxPos = positions[positions.length-1]
 
-    console.log(minPos, maxPos)
-
     const pwCost = positions.map(v => [v, positionCost(v, positions)]).sort(([_,a], [__,b]) => (a - b))
-
-    console.log(pwCost)
 
     solution.innerText = `Position: ${pwCost[0][0]} Cost: ${pwCost[0][1]}`
 
+    const part2Range = Array(maxPos+minPos).fill(0).map((_,i) => i+minPos)
+
+    const npwCost = part2Range.map(v => [v, newPositionCost(v, positions)]).sort(([_,a], [__,b]) => (a-b))
+
+    solution.innerText += `\nPosition: ${npwCost[0][0]} Cost: ${npwCost[0][1]}`
 }
