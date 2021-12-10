dc16b64ef048ffb103a83dc763523e7ce213dc7a
Benedict Cairns <bcairns21@gmail.com>
2021-12-10 08:02:46 +0000

    Day 10 visualisation attempt

--- 2021/10/10.js
+++ 2021/10/10.js
@@ -3,13 +3,45 @@ const solveBtn = document.getElementById('solvepuzzle')
 const solution = document.getElementById('solutionoutput')
 const visual = document.getElementById('visual')
 
+/** @param {Node} elem */
+const emptyNode = (elem) => {
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild)
+    }
+}
+
+/**
+ * @param {[boolean, number][]} scores 
+ * @param {number} width 
+ * @param {number} height 
+ */
+const graphScores = (scores, width, height) => {
+    const barHeight = 5
+    const totalWidth = 500
+    const canvas = document.createElement('canvas')
+    // Use a width ratio otherwise way too big
+    canvas.width = totalWidth
+    canvas.height = height*barHeight
+
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'purple'
+
+    scores.forEach(([a,b], i) => {
+        ctx.fillStyle = a ? 'green' : 'red'
+        ctx.fillRect(0, i*barHeight, (b/width) * totalWidth, barHeight)
+    })
+    ctx.fillRect(0,0,10,10)
+    return canvas
+}
+
 const closers = {
     '(': ')',
     '<': '>',
     '{': '}',
     '[': ']'
 }
-
 
 const score = {
     ')': 3,
@@ -53,6 +85,7 @@ const checkLine = (line) => {
 }
 
 solveBtn.onclick = () => {
+    /** @type {[[string]]} */
     const lines = puzzleInput.value.trim().split('\n').map(l => l.split(''))
 
     const scores = lines.map(checkLine)
@@ -65,4 +98,11 @@ solveBtn.onclick = () => {
     const medianScore = completeScores[Math.floor(completeScores.length / 2)]
 
     solution.innerText += `\nFile Autocompletion Score: ${medianScore}`
+
+    // Visualise
+    emptyNode(visual)
+    const height = scores.length
+    const width = scores.map(([_a,b]) => b).sort((a,b) => a-b).reverse()[0]
+    visual.append(graphScores(scores, width, height))
+
 }
